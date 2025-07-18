4f5cbab96d7e935748b90e28dde3c60d5d766032
tramtran-helen <tramtran.helen@gmail.com>
2025-07-18 12:50:36 -0400

    update ShowBook

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
 import { BsArrowLeft } from 'react-icons/bs'
 
+
+/*Create BackButton that navigates users to different routes
+If nothing is passed, it uses the default '/'
+The actual destination value will be passed later*/
 const BackButton = ({ destinations = '/' }) => {
     return(
-        <div className='flex'>
-            <Link to={destination} 
+        <div className="flex">
+            <Link to={destinations} 
             className="bg-blue-200 hover:bg-blue-400 text-blue-800 font-semibold py-2 px-4 rounded-md shadow-sm transition duration-200">
-                <BsArrowLeft className="text-2x1"/>
+                <BsArrowLeft className="text-2xl"/>
             </Link>
         </div>
     )
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,8 +1,34 @@
-import react from 'react'
+import react, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton.jsx'
+import Spinner from '../components/Spinner.jsx'
+
 
 const ShowBook = () => {
+    const [book, setBook] = useState({})
+    const [loading, setLoading] = useState(false)
+    const {id} = useParams() 
+
+
+    useEffect(() => {
+        setLoading(true)
+        axios
+            .get(`http://localhost:5555/books/${id}`)
+            .then((res) => {
+                setBook(res.data)
+                setLoading(false)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+
     return(
-        <div>ShowBook</div>
+        <div className='p-6 min-h-screen bg-gradient-to-b from-sky-50 via-teal-50 to-sky-50'>
+
+        </div>
     )
 }
 
